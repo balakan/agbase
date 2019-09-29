27d858f8faafc1b1b2974b090ad2168fb330fcc6
Filip Jankoski <filip@agmedia.hr>
2019-09-29 02:53:43 +0200

    Page Builder & AdminEditor specification.

--- resources/views/back/settings/pages/index.blade.php
+++ resources/views/back/settings/pages/index.blade.php
@@ -5,18 +5,41 @@
     <div class="content">
         <div class="my-50 text-center">
             <h2 class="font-w700 text-black mb-10">Pages</h2>
-            <h3 class="h5 text-muted mb-0">Welcome to your app.</h3>
+            <h3 class="h5 text-muted mb-0">Kako izraditi jednostavan Page Builder i Admin Editor?</h3>
         </div>
         <div class="row justify-content-center">
-            <div class="col-md-6 col-xl-5">
+            <div class="col-md-8 col-xl-8">
                 <div class="block">
                     <div class="block-content">
                         <p class="text-muted">
-                            We’ve put everything together, so you can start working on your Laravel project as soon as possible! Codebase assets are integrated and work seamlessly with Laravel Mix, so you can use the npm scripts as you would in any other Laravel project.
+                            Treba nam u bazi osnovna tablica za Page + tablica za PageSegment
                         </p>
-                        <p class="text-muted">
-                            Feel free to use any examples you like from the full versions to build your own pages. <strong>Wish you all the best and happy coding!</strong>
-                        </p>
+                        <h4>Page tablica <small>npr...</small></h4>
+                        <ul>
+                            <li>id</li>
+                            <li>title</li>
+                            <li>description</li>
+                            <li>meta_title</li>
+                            <li>meta_description</li>
+                            <li>base_theme</li>
+                            <li>top_menu <small> - 0/1</small></li>
+                            <li>...</li>
+                            <li>sort_order</li>
+                        </ul>
+                        <h4>PageSegment tablica <small>npr...</small></h4>
+                        <ul>
+                            <li>id</li>
+                            <li>page_id</li>
+                            <li>title</li>
+                            <li>title_align</li>
+                            <li>description</li>
+                            <li>description_align</li>
+                            <li>image</li>
+                            <li>image_align</li>
+                            <li>segment <small> - 'slider', 'carusel', 'rich_text', 'call_to_action',</small></li>
+                            <li>...</li>
+                            <li>sort_order</li>
+                        </ul>
                     </div>
                 </div>
             </div>
